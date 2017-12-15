4634dea71c1169e086a8dca8b77699a20652cf15
Schiva <schiavonjp@yahoo.com.br>
2017-12-15 05:16:49 -0200

    traffic add

---
+++ Jogo CG/Assets/Scripts/Carro.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Carro : MonoBehaviour {
+
+	public Transform[] pontos;
+	public int contPontos;
+	public NavMeshAgent nav;
+	public float velocidadeNormal,distancia;
+	public GameObject ignore;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	void OnCollisionEnter (Collision  collision) {
+	 
+	    if (collision.gameObject.tag == "theobjectToIgnore") {
+	    	Physics.IgnoreCollision(nav.GetComponent<Collider>(), GetComponent<Collider>());
+	       }
+	 
+	}
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (pontos [contPontos].position);
+		distancia = Vector3.Distance (transform.position, pontos [contPontos].position);
+		if (distancia < 1) {
+			contPontos++;
+		}
+		if (Physics.Raycast (transform.position, transform.forward, 8)) {
+			nav.speed = 0;
+		} else {
+			nav.speed = velocidadeNormal;
+		}
+		if (contPontos == pontos.Length) {
+			contPontos = 0;
+		}
+	}
+}
---
+++ Jogo CG/Assets/Scripts/Farol.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Farol : MonoBehaviour {
+	public GameObject vermelho, verde, amarelo,trigger;
+	public float tempo_vermelho,tempo_verde, tempo_amarelo,tempo;
+	public Material cor;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (tempo < tempo_verde) {
+			tempo += Time.deltaTime;
+		} else {
+			tempo = 0;
+		}
+
+		if (tempo < tempo_vermelho) {
+			vermelho.GetComponent<Renderer> ().material.EnableKeyword ("_EMISSION");
+			amarelo.GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+			verde.GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+			trigger.SetActive (true);
+		}
+		if (tempo > tempo_vermelho && tempo < tempo_amarelo) {
+			vermelho.GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+			amarelo.GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+			verde.GetComponent<Renderer> ().material.EnableKeyword ("_EMISSION");
+			trigger.SetActive (false);
+		}
+		if (tempo > tempo_amarelo) {
+			vermelho.GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+			amarelo.GetComponent<Renderer> ().material.EnableKeyword ("_EMISSION");
+			verde.GetComponent<Renderer> ().material.DisableKeyword ("_EMISSION");
+			trigger.SetActive (true);
+		}
+	}
+}
